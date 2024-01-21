2e153e17b68ce9bf9ef1f25639b7c7edfb0ab838
Ashu Jha <ashujha4444@gmail.com>
2024-01-22 01:24:53 +0530

    Update WC.js

--- WC.js
+++ WC.js
@@ -1,1 +1,11 @@
+let button = document.getElementById('Calculatebtn');
+
+button.addEventListener('click', function(){
+    let word = document.getElementById('str').value;
+    let count = word.length;
+    let outputDiv = document.getElementById('output');
+
+    outputDiv.innerHTML = `<h1>${count}</h1>`
+});
+
 
