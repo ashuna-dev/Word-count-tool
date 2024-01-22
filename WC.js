e9894f3d3ec49a9e123077547a5d5c647d29a514
Ashu Jha <ashujha4444@gmail.com>
2024-01-22 20:17:19 +0530

    Update WC.js

--- WC.js
+++ WC.js
@@ -1,11 +1,10 @@
-let button = document.getElementById('Calculate');
-
-button.addEventListener('click', function(){
-    let word = document.getElementById('wordinput').value;
-    let count = word.length;
-    let outputDiv = document.getElementById('output');
-
-    outputDiv.innerHTML = `<h1>${count}</h1>`
+document.addEventListener('DOMContentLoaded', function() {
+    let button = document.getElementById('calculate');
+    button.addEventListener('click', function(){
+        let word = document.getElementById('wordInput').value;
+        let count = word.length;
+        let outputDiv = document.getElementById('output');
+        outputDiv.innerHTML = `<h1>${count}</h1>`;
+    });
 });
-
 
